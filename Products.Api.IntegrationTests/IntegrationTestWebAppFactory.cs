b290f8a0ac0f4ea2c318f00eb48b06da4978bcc4
nlespiaucq <n.lespiaucq@betclicgroup.com>
2024-02-15 19:02:45 +0100

    adjust code for using service containers

--- Products.Api.IntegrationTests/IntegrationTestWebAppFactory.cs
+++ Products.Api.IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -11,34 +11,33 @@ namespace Application.IntegrationTests;
 public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
     private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
-        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-        .WithPassword("Strong_password_123!")
+        .WithDatabase({ Password = "Strong_password_123!" })
         .Build();
 
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
+protected override void ConfigureWebHost(IWebHostBuilder builder)
+{
+    builder.ConfigureTestServices(services =>
     {
-        builder.ConfigureTestServices(services =>
+        var descriptor = services
+            .SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+        if (descriptor is not null)
         {
-            var descriptor = services
-                .SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+            services.Remove(descriptor);
+        }
 
-            if (descriptor is not null)
-            {
-                services.Remove(descriptor);
-            }
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(_dbContainer.GetConnectionString()));
+    });
+}
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(_dbContainer.GetConnectionString()));
-        });
-    }
+public Task InitializeAsync()
+{
+    return _dbContainer.StartAsync();
+}
 
-    public Task InitializeAsync()
-    {
-        return _dbContainer.StartAsync();
-    }
-
-    public new Task DisposeAsync()
-    {
-        return _dbContainer.StopAsync();
-    }
+public new Task DisposeAsync()
+{
+    return _dbContainer.StopAsync();
+}
 }
